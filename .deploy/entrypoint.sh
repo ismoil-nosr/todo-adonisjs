d77dbf5b521c892b42588e6ce4791e42c77d9b35
Ismoil Nosr <ismoil.nosr@gmail.com>
2020-12-06 19:56:15 +0500

    zb

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -2,11 +2,13 @@
 
 echo "🎬 entrypoint.sh"
 npm i
-cp $APP_PATH/.env.example $APP_PATH/.env
+ls
 
-npm run-script build
-cp $APP_PATH/.env $APP_PATH/build/.env
+# cp $APP_PATH/.env.example $APP_PATH/.env
+
+# npm run-script build
+# cp $APP_PATH/.env $APP_PATH/build/.env
 
 echo "🎬 node commands"
 
-node $APP_PATH/build/server.js
+# node $APP_PATH/build/server.js
